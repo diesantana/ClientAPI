53e6968def0c2c9aca4c980737c5d713a489706a
Diego Santana <3c.santana@gmail.com>
2024-04-12 20:20:25 -0300

    feat: Criação do ControllerAdvice para captura de erros

---
+++ src/main/java/com/devsuperior/ClientAPI/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.devsuperior.ClientAPI.controllers.handlers;
+
+import com.devsuperior.ClientAPI.dto.CustomErrorDTO;
+import com.devsuperior.ClientAPI.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorDTO> ResourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
